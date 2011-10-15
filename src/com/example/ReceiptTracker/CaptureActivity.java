115caf016cd90fc5facee9b48b8ea00d66caf038
bryanl <bryan@osesm.com>
2011-10-15 14:12:13 -0400

    Show a preview from the camera

---
+++ src/com/example/ReceiptTracker/CaptureActivity.java
@@ -0,0 +1,67 @@
+package com.example.ReceiptTracker;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class CaptureActivity extends Activity {
+
+	private CameraPreview mPreview;
+	Camera mCamera;
+	int numberOfCameras;
+	int cameraCurrentlyLocked;
+
+	// The first rear facing camera
+	int defaultCameraId;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		mPreview = new CameraPreview(this);
+		setContentView(mPreview);
+
+		// Find the total number of cameras available
+		numberOfCameras = Camera.getNumberOfCameras();
+
+		// Find the ID of the default camera
+		CameraInfo cameraInfo = new CameraInfo();
+		for (int i = 0; i < numberOfCameras; i++) {
+			Camera.getCameraInfo(i, cameraInfo);
+			if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+				defaultCameraId = i;
+			}
+		}
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// Open the default i.e. the first rear facing camera.
+		mCamera = Camera.open();
+		cameraCurrentlyLocked = defaultCameraId;
+		mPreview.setCamera(mCamera);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		// Because the Camera object is a shared resource, it's very
+		// important to release it when the activity is paused.
+		if (mCamera != null) {
+			mPreview.setCamera(null);
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+
+}
--- src/com/example/ReceiptTracker/ReceiptTrackerActivity.java
+++ src/com/example/ReceiptTracker/ReceiptTrackerActivity.java
@@ -1,13 +1,28 @@
 package com.example.ReceiptTracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ReceiptTrackerActivity extends Activity {
-    /** Called when the activity is first created. */
+
+	private View.OnClickListener snapListener;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+                
+        snapListener = new View.OnClickListener() {    		
+    		public void onClick(View v) {
+    			Intent intent = new Intent(ReceiptTrackerActivity.this, CaptureActivity.class);
+    			startActivity(intent);
+    		}
+    	};
+    	
+    	ImageButton snap = (ImageButton) findViewById(R.id.snap);
+    	snap.setOnClickListener(snapListener);
     }
 }
