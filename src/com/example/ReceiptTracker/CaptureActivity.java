616081a855edb554cab6c7f944011138da8ae22c
bryanl <bryan@osesm.com>
2011-10-15 14:55:07 -0400

    Prepare the picture to be saved

--- src/com/example/ReceiptTracker/CaptureActivity.java
+++ src/com/example/ReceiptTracker/CaptureActivity.java
@@ -3,9 +3,16 @@ package com.example.ReceiptTracker;
 import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class CaptureActivity extends Activity {
 
@@ -16,6 +23,8 @@ public class CaptureActivity extends Activity {
 
 	// The first rear facing camera
 	int defaultCameraId;
+	
+	byte[] tempdata;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,22 @@ public class CaptureActivity extends Activity {
 		// Find the total number of cameras available
 		numberOfCameras = Camera.getNumberOfCameras();
 
+		LayoutInflater inflater = LayoutInflater.from(this);
+		View cameraOverlay = inflater.inflate(R.layout.camera_overlay, null);
+		addContentView(cameraOverlay, new LayoutParams(
+				LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+
+		View.OnClickListener snapButtonListener = new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Log.d("CameraPreview", "snapped a picture");
+			}
+		};
+
+		Button snapButton = (Button) findViewById(R.id.snapButton);
+		snapButton.setOnClickListener(snapButtonListener);
+
 		// Find the ID of the default camera
 		CameraInfo cameraInfo = new CameraInfo();
 		for (int i = 0; i < numberOfCameras; i++) {
@@ -41,6 +66,32 @@ public class CaptureActivity extends Activity {
 
 	}
 
+	ShutterCallback mShutterCallback = new ShutterCallback() {
+
+		@Override
+		public void onShutter() {
+		}
+	};
+	
+	PictureCallback mPictureCallback = new PictureCallback() {		
+		public void onPictureTaken(byte[] data, Camera c) {} 
+	};
+		
+	
+	PictureCallback mjpeg = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera c) {
+			if (data !=null) {
+				tempdata = data;
+				done();
+			}
+		}
+	};
+	
+	private void done() {
+		// 1. save picture in database
+		// 2. forward to Receipt Activity (populate bundle with receipt id)
+	}
+	
 	@Override
 	protected void onResume() {
 		super.onResume();
